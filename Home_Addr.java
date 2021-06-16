dcaca50932bd417b20ab391d6f3b1702156d5a48
venkat143-v <81428675+venkat143-v@users.noreply.github.com>
2021-06-16 16:21:10 +0530

    Add files via upload

---
+++ Home_Addr.java
@@ -0,0 +1,14 @@
+public class Home_Addr {
+	public static void main(String[] args) {
+		String name = "Venkat";
+		String dno = "2-12/1";
+		String city = "Velangi";
+		String state = "Andhra Pradesh";
+		System.out.println("Address :");
+		System.out.println("---------");
+		System.out.println("Name = "+name);
+		System.out.println("DNO = "+dno);
+		System.out.println("City = "+city);
+		System.out.println("State = "+state);
+	}
+}
